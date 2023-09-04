4c27d97db7f23325ecf2ec65259e38529d0e7bbd
Mimonstr <p-mishka1@mail.ru>
2023-09-04 10:28:08 +0300

    Annotation part 3.3: @Value with file properties

--- src/main/java/com/paryshkin/SpringCourseAlishev/MusicPlayer.java
+++ src/main/java/com/paryshkin/SpringCourseAlishev/MusicPlayer.java
@@ -2,12 +2,38 @@ package com.paryshkin.SpringCourseAlishev;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer
 {
-   private Music music1;
+    @Value("${musicPlayer.name}")
+    private String name;
+    @Value("${musicPlayer.volume}")
+    private int volume;
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public int getVolume()
+    {
+        return volume;
+    }
+
+    public void setVolume(int volume)
+    {
+        this.volume = volume;
+    }
+
+    private Music music1;
    private Music music2;
 
     @Autowired

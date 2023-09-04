218562c03acea2b9917991773a383c3f376c1fe9
Mimonstr <p-mishka1@mail.ru>
2023-09-04 11:19:07 +0300

    JavaClass annotations: main: create AnnotationConfigApplicationContext

--- src/main/java/com/paryshkin/SpringCourseAlishev/TestSpring.java
+++ src/main/java/com/paryshkin/SpringCourseAlishev/TestSpring.java
@@ -1,26 +1,13 @@
 package com.paryshkin.SpringCourseAlishev;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring
 {
     public static void main(String[] args)
     {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-
-//        Music music = context.getBean("rockMusic", Music.class);
-//        MusicPlayer musicPlayer = new MusicPlayer(music);
-//        musicPlayer.playMusic();
-//
-//        Music music2 = context.getBean("classicalMusic", ClassicalMusic.class);
-//        MusicPlayer musicPlayer2 = new MusicPlayer(music2);
-//        musicPlayer2.playMusic();
-
-//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-//        musicPlayer.playMusic();
-
-//        Computer computer = context.getBean("computer", Computer.class);
-//        System.out.println(computer);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
 
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         System.out.println(musicPlayer.getName());
